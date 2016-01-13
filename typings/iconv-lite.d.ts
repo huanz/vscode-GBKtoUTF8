f5fc16a3f475a38b2d4d26710747911ca4b96acf
yuhuanzhu <yuhuanzhu@baidu.com>
2016-01-13 17:34:11 +0800

    add iconv-lite declare

---
+++ typings/iconv-lite.d.ts
@@ -0,0 +1,13 @@
+/// <reference path='./node.d.ts'/>
+
+declare module 'iconv-lite' {
+	export function decode(buffer: NodeBuffer, encoding:string, options?:any): string;
+
+	export function encode(content:string, encoding:string, options?:any): NodeBuffer;
+
+	export function encodingExists(encoding:string): boolean;
+
+	export function decodeStream(encoding:string): NodeJS.ReadWriteStream;
+
+	export function encodeStream(encoding:string): NodeJS.ReadWriteStream;
+}
